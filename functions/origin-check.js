604ada3c26b2482157e1ac4ddbf624bdd4cfdc9d
sigmabucket12333 <nosweet68@gmail.com>
2025-01-30 00:21:03 +0000

    Update origin-check.js

--- functions/origin-check.js
+++ functions/origin-check.js
@@ -13,7 +13,7 @@ export default async (request, context) => {
   const isRobloxStudioPlayMode = xRobloxStudio === 'true'; // Custom header that can be set during Play mode
 
   if (isRobloxGameClient) {
-    return new Response('122811973378971', {
+    return new Response('71196479846849', {
       status: 200,
       headers: { 'Content-Type': 'text/plain' } // Plain text response
     });
